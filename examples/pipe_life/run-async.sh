f0c003010c114eff63dbb7b24808c99a8bc9e599
Kevin Cameron <cameron.eda@gmail.com>
2026-03-02 18:30:03 -0800

    Update

--- examples/pipe_life/run-async.sh
+++ examples/pipe_life/run-async.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-NVC=/usr/local/src/nvc-build/bin/nvc
+BUILD_DIR=`echo $PWD | sed s=nvc.*=nvc-build=`
+NVC=nvc
+PATH=$BUILD_DIR/bin:$PATH
+which nvc 
+which nvc >/dev/null 2>&1
+if [ 0 != $? ] ; then
+    echo No nvc in \$PATH
+    exit
+fi
 rm -rf work
 $NVC --std=2040 -L /usr/local/src/nvc-build/lib/ --work=work -a life_cell.vhd life_top.vhd
 $NVC --std=2040 -L /usr/local/src/nvc-build/lib/ -e life_top
+clear
 $NVC -r life_top
